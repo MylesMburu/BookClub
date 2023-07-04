6ec1b7a7b78e9acc414ada248b0226c8130aad1c
MylesMburu <ngicumyles@gmail.com>
2023-07-04 14:35:38 +0300

    catalogue added

---
+++ src/components/Catalogue/BookData.js
@@ -0,0 +1,155 @@
+const books = 
+[
+    {
+      "id": 1,
+      "name": "The Great Gatsby",
+      "author": "F. Scott Fitzgerald"
+    },
+    {
+      "id": 2,
+      "name": "To Kill a Mockingbird",
+      "author": "Harper Lee"
+    },
+    {
+      "id": 3,
+      "name": "1984",
+      "author": "George Orwell"
+    },
+    {
+      "id": 4,
+      "name": "Pride and Prejudice",
+      "author": "Jane Austen"
+    },
+    {
+      "id": 5,
+      "name": "The Catcher in the Rye",
+      "author": "J.D. Salinger"
+    },
+    {
+      "id": 6,
+      "name": "Moby-Dick",
+      "author": "Herman Melville"
+    },
+    {
+      "id": 7,
+      "name": "Harry Potter and the Sorcerer's Stone",
+      "author": "J.K. Rowling"
+    },
+    {
+      "id": 8,
+      "name": "The Lord of the Rings",
+      "author": "J.R.R. Tolkien"
+    },
+    {
+      "id": 9,
+      "name": "Alice's Adventures in Wonderland",
+      "author": "Lewis Carroll"
+    },
+    {
+      "id": 10,
+      "name": "The Hobbit",
+      "author": "J.R.R. Tolkien"
+    },
+    {
+      "id": 11,
+      "name": "Brave New World",
+      "author": "Aldous Huxley"
+    },
+    {
+      "id": 12,
+      "name": "The Chronicles of Narnia",
+      "author": "C.S. Lewis"
+    },
+    {
+      "id": 13,
+      "name": "The Odyssey",
+      "author": "Homer"
+    },
+    {
+      "id": 14,
+      "name": "Fahrenheit 451",
+      "author": "Ray Bradbury"
+    },
+    {
+      "id": 15,
+      "name": "Jane Eyre",
+      "author": "Charlotte Brontë"
+    },
+    {
+      "id": 16,
+      "name": "The Great Expectations",
+      "author": "Charles Dickens"
+    },
+    {
+      "id": 17,
+      "name": "The Adventures of Huckleberry Finn",
+      "author": "Mark Twain"
+    },
+    {
+      "id": 18,
+      "name": "Wuthering Heights",
+      "author": "Emily Brontë"
+    },
+    {
+      "id": 19,
+      "name": "The Alchemist",
+      "author": "Paulo Coelho"
+    },
+    {
+      "id": 20,
+      "name": "Gone with the Wind",
+      "author": "Margaret Mitchell"
+    },
+    {
+      "id": 21,
+      "name": "One Hundred Years of Solitude",
+      "author": "Gabriel García Márquez"
+    },
+    {
+      "id": 22,
+      "name": "The Kite Runner",
+      "author": "Khaled Hosseini"
+    },
+    {
+      "id": 23,
+      "name": "The Picture of Dorian Gray",
+      "author": "Oscar Wilde"
+    },
+    {
+      "id": 24,
+      "name": "Don Quixote",
+      "author": "Miguel de Cervantes"
+    },
+    {
+      "id": 25,
+      "name": "The Scarlet Letter",
+      "author": "Nathaniel Hawthorne"
+    },
+    {
+      "id": 26,
+      "name": "The Count of Monte Cristo",
+      "author": "Alexandre Dumas"
+    },
+    {
+      "id": 27,
+      "name": "Crime and Punishment",
+      "author": "Fyodor Dostoevsky"
+    },
+    {
+      "id": 28,
+      "name": "The Brothers Karamazov",
+      "author": "Fyodor Dostoevsky"
+    },
+    {
+      "id": 29,
+      "name": "The Grapes of Wrath",
+      "author": "John Steinbeck"
+    },
+    {
+      "id": 30,
+      "name": "Lord of the Flies",
+      "author": "William Golding"
+    }
+  ]
+  
+  export default books;
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import {FiPhoneCall} from 'react-icons/fi'
+// import 'dotenv/config';
+
 
 export const Contacts = () => {
+  
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    try {
+      emailjs.sendForm('service_b4yrlg9','template_8qnuo6d',form.current,'BuzQ1LqDL1lsJ2zC4');
+    } catch (error) {
+      
+    }
+    e.target.reset();
+    window.alert('Message sent');
+
+  }
   return (
     <div className='bg-primary text-white'>
       <div className='flex flex-col px-6 pt-8 justify-center items-center'>
@@ -8,8 +26,13 @@ export const Contacts = () => {
           <h2 className='border-b-2 border-white pb-1'>Contact Us</h2>
         </div>
         <div>
-          <form className="flex flex-col p-8 w-full md:w-96 text-black">
-            <h3 className='text-white'>Phone: +254759129028</h3>
+          <form className="flex flex-col p-8 w-full md:w-96 text-black" ref={form} onSubmit={sendEmail}>
+          <div className="flex gap-4 bg-secondary p-3 rounded mb-4">
+            <FiPhoneCall className='mt-1'/>
+            <h3>
+              +(254)759129028
+            </h3>
+          </div>
             {/* Input for full name */}
             <input
               type="text"
