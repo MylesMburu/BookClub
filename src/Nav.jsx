52357100e467f4d9da4edb4f4fb1057cbd41e8b5
MylesMburu <ngicumyles@gmail.com>
2023-07-10 15:50:45 +0300

    complete responsiveness

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,10 +6,14 @@ export const Nav = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className='flex md:flex-row justify-between p-2 bg-secondary text-primary'>
-      <div id='logo' className='text-xl font-bold'>
-        Book Club
-      </div>
+    <div className='flex md:flex-row justify-between p-4 h-full md:h-16 bg-secondary text-primary'>
+      {/* Conditionally render the logo */}
+      {!open && (
+        <div id='logo' className='text-xl font-bold'>
+          Book Club
+        </div>
+      )}
+
       <div className='flex md:flex-row gap-4 font-semibold'>
         <div className='md:hidden'>
           {open ? (
@@ -18,7 +22,7 @@ export const Nav = () => {
             <FiMenu className='text-primary cursor-pointer' onClick={() => setOpen(!open)} />
           )}
         </div>
-        <div className={`md:flex ${open ? 'flex flex-col' : 'hidden'}`}>
+        <div className={`md:flex ${open ? 'flex flex-col ml-24 gap-2' : 'hidden'}`}>
           <NavLink
             to='/'
             className='nav-hov md:ml-4'
