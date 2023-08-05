3549325f30210728243c01d5524d29d2213e4b6c
MylesMburu <ngicumyles@gmail.com>
2023-08-05 09:55:57 +0300

    styled projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,32 +6,32 @@ export const Projects = () => {
     
     <div className='bg-primary'>
       <h1 className='text-center text-4xl text-white font-bold'>Projects</h1>
-      <div className='flex flex-col gap-4 mx-4 my-6 md:grid-cols-2 md:grid md:px-2 justify-center items-center'>
+      <div className='flex flex-col gap-4 mx-4 py-6 md:grid-cols-2 md:grid md:px-2 justify-center items-center'>
         {projects.map((project, index) => {
           return (
             <div
               key={index}
-              className='flex flex-col justify-center items-center bg-secondary rounded-md p-4'
+              className='flex flex-col lg:grid justify-center lg:grid-cols-2 items-center bg-secondary rounded-md p-4 lg:p-0'
             >
-              <img
-                src={project.image}
-                alt={project.name}
-                className='rounded-md object-cover w-72 h-64 md:w-96 md:h-72'
-              />
-              <div>
-                <div>
-                  <h2 className='text-xl font-bold'>{project.institution}</h2>
-                  <p className='text-center'>{project.date}</p>
-                  <div className='flex gap-4'>
-                    <a
-                      href={project.live}
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      className='px-6 py-2 text-white bg-red-600 hover:bg-red-700 rounded'
-                    >
-                      Learn More
-                    </a>
-                  </div>
+              <div className=''>
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  className='rounded-md lg:rounded-sm object-cover w-72 h-64 md:w-96 md:h-72'
+                />
+              </div>
+              <div className='lg:p-14'>
+                <h2 className='text-xl font-bold'>{project.institution}</h2>
+                <p className='text-left'>{project.date}</p>
+                <div className='flex gap-4'>
+                  <a
+                    href={project.live}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='px-6 py-2 text-white bg-red-600 hover:bg-red-700 rounded'
+                  >
+                    Learn More
+                  </a>
                 </div>
               </div>
             </div>
